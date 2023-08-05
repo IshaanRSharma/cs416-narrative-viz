aa867ed043ac56a3a5b99aa3b38a95ab887c8be4
Ishaan Sharma <ishaansharma@Ishaans-MacBook-Pro-2.local>
2023-08-05 00:10:54 -0400

    test

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slideData = [
+        { title: "Slide 1", content: "Content for slide 1" },
+        { title: "Slide 2", content: "Content for slide 2" },
+        { title: "Slide 3", content: "Content for slide 3" },
+    ];
+    
+    let currentSlide = 0;
+    
+    const slideContainer = d3.select("#slide-container");
+    
+    function updateSlide() {
+        const slide = slideData[currentSlide];
+        slideContainer.html(`
+            <h2>${slide.title}</h2>
+            <p>${slide.content}</p>
+        `);
+    }
+    
+    updateSlide();
+    
+    d3.select("#previous").on("click", function() {
+        if (currentSlide > 0) {
+            currentSlide--;
+            updateSlide();
+        }
+    });
+    
+    d3.select("#next").on("click", function() {
+        if (currentSlide < slideData.length - 1) {
+            currentSlide++;
+            updateSlide();
+        }
+    });
+});
