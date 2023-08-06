7704e8e14c5fba57206a5b337d273411f6a0712f
Ishaan Sharma <ishaansharma@Ishaans-MacBook-Pro-2.local>
2023-08-06 18:27:21 -0400

    test

--- script.js
+++ script.js
@@ -1,55 +1,90 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slideData = [
-        {  title: "Diabetic Risk Factors", content: "D3", type: "d3" },
-        { title: "Slide 2", content: "Content for slide 2" },
-        { title: "Slide 3", content: "Content for slide 3" },
-    ];
-    
-    let currentSlide = 0;
-    
-    const slideContainer = d3.select("#slide-container");
-    const startButton = document.getElementById('start');
-    
-    function updateSlide() {
-        const slide = slideData[currentSlide];
-    
-        if (slide.type === "d3") {
-            renderD3Visualization();
-    
-        } else {
-            slideContainer.html(`
-                <h2>${slide.title}</h2>
-                <p>${slide.content}</p>
-            `);
-        }
-        
-        if(currentSlide === slideData.length - 1) {
-            startButton.style.display = 'inline';
-        } else {
-            startButton.style.display = 'none';
-        }
-    }
-    
-    updateSlide();
-    
-    d3.select("#previous").on("click", function() {
-        if (currentSlide > 0) {
-            currentSlide--;
-            updateSlide();
-        }
-    });
-    
-    d3.select("#next").on("click", function() {
-        if (currentSlide < slideData.length - 1) {
-            currentSlide++;
-            updateSlide();
-        }
-    });
+let currentScene = 0;
+const scenes = [renderScene1, renderScene2, renderScene3];  
 
-    function renderD3Visualization() {
-        // Load the CSV file using D3
-  
-        
 
+window.onload = renderScene;
+document.getElementById('previous').style.display = 'none';
+
+document.getElementById('next').addEventListener('click', function() {
+    if (currentScene === scenes.length - 1) {
+        document.getElementById('next').style.display = 'none';
+        document.getElementById('start-over').style.display = 'inline-block'; 
+    } else {
+        currentScene = (currentScene + 1) % scenes.length;
+        renderScene();
+
+        if (currentScene === 1) {
+            document.getElementById('previous').style.display = 'inline-block'; 
+        }
     }
 });
+
+document.getElementById('previous').addEventListener('click', function() {
+    if (currentScene === 1) {
+        document.getElementById('previous').style.display = 'none';
+    }
+
+    currentScene = (currentScene - 1 + scenes.length) % scenes.length;
+    renderScene();
+});
+
+document.getElementById('start-over').addEventListener('click', function() {
+    currentScene = 0;
+    renderScene();
+    
+    document.getElementById('start-over').style.display = 'none';
+    document.getElementById('previous').style.display = 'none';
+    document.getElementById('next').style.display = 'inline-block'; 
+});
+
+
+function clearScene() {
+    d3.select("#slide-container svg").selectAll("*").remove();
+}
+
+function renderScene() {
+    clearScene();
+    printAges();
+    scenes[currentScene]();
+}
+
+function renderScene1() {
+    const svg = d3.select("#slide-container svg");
+    // D3 code for the first scene.
+    svg.append("rect")
+       .attr("x", 50)
+       .attr("y", 50)
+       .attr("width", 200)
+       .attr("height", 100)
+       .attr("fill", "#FFD700");
+}
+
+function renderScene2() {
+    const svg = d3.select("#slide-container svg");
+    // D3 code for the second scene.
+    svg.append("circle")
+       .attr("cx", 150)
+       .attr("cy", 150)
+       .attr("r", 70)
+       .attr("fill", "#4CAF50");
+}
+
+function renderScene3() {
+    const svg = d3.select("#slide-container svg");
+    // D3 code for the third scene.
+    svg.append("ellipse")
+       .attr("cx", 250)
+       .attr("cy", 150)
+       .attr("rx", 100)
+       .attr("ry", 50)
+       .attr("fill", "#FF5733");
+}
+
+function printAges() {
+    d3.csv("Data/data.csv").then(function(data) {
+        data.forEach(d => {
+            console.log(d.Age);
+        });
+    });
+}
+
