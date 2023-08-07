8aad9bc2e81fd6519a38f662aecb89e81344227b
Ishaan Sharma <ishaansharma@Ishaans-MacBook-Pro-2.local>
2023-08-07 11:11:43 -0400

    change dev

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ function renderScene1(raw_data) {
         width = +svg.attr("width"),
         height = +svg.attr("height");
 
-    var xScale = d3.scalePoint()
+        var xScale = d3.scaleBand()
         .domain(aggregatedData.map(d => d.genre))
-        .range([20, width - 20])
+        .range([50, width - 50])
         .padding(0.5);
 
     var yScale = d3.scaleLinear()
@@ -78,24 +78,23 @@ function renderScene1(raw_data) {
         .attr("transform", "translate(50,0)")
         .call(yAxis);
 
-    // Draw the scatterplot
-    var circles = svg.selectAll("circle")
-        .data(raw_data)
-        .enter().append("circle")
-        .attr("cx", d => xScale(d.genre))
-        .attr("cy", d => yScale(+d.energy))
-        .attr("r", 5)
+        var bars = svg.selectAll("rect")
+        .data(aggregatedData)
+        .enter().append("rect")
+        .attr("x", d => xScale(d.genre))
+        .attr("y", d => yScale(d.averageEnergy))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => height - 50 - yScale(d.averageEnergy))
         .style("fill", "#0077b6")
         .on("mouseover", function(event, d) {
             d3.select(this)
-                .attr("r", 7)
                 .style("fill", "#ff5733");
-
+    
             // Add tooltip
             svg.append("text")
                 .attr("id", "tooltip")
-                .attr("x", xScale(d.genre))
-                .attr("y", yScale(+d.energy) - 15)
+                .attr("x", xScale(d.genre) + xScale.bandwidth() / 2)
+                .attr("y", yScale(d.averageEnergy) - 15)
                 .attr("text-anchor", "middle")
                 .attr("font-family", "sans-serif")
                 .attr("font-size", "11px")
@@ -105,13 +104,11 @@ function renderScene1(raw_data) {
         })
         .on("mouseout", function(d) {
             d3.select(this)
-                .attr("r", 5)
                 .style("fill", "#0077b6");
-
+    
             // Remove tooltip
             d3.select("#tooltip").remove();
         });
-
     // Add annotation
     const annotations = [{
         note: {
