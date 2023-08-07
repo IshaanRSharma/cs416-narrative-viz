b778f1ec7b65b241da42ecc59c054ce4143f4c8b
Ishaan Sharma <ishaansharma@Ishaans-MacBook-Pro-2.local>
2023-08-07 19:50:56 -0400

    dsmsfkfmsdkwdfsdsdsdfdfssdfdfsdfff

--- script.js
+++ script.js
@@ -426,3 +426,10 @@ function clearScene3Content() {
     d3.selectAll("button").style("display", "none");
 }
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];  // Swap elements
+    }
+    return array;
+}
