497dc7b7410fef59d0933c0998466a8d6c33d3f0
Ishaan Sharma <ishaansharma@Ishaans-MacBook-Pro-2.local>
2023-08-06 20:58:44 -0400

    scene 1as

--- script.js
+++ script.js
@@ -52,60 +52,57 @@ function renderScene() {
 }
 
 function preprocessData(rawData) {
-    // Filter out rows with NaN values
-    const filteredData = rawData.filter(d => !isNaN(d.Diabetes_012) && !isNaN(d.Sex) && !isNaN(d.Age));
-
-    const ageBins = ["0-20", "20-40", "40-60", "60-80", "80-100"];
-
-    // Initialize an empty data structure for our age bins and sexes
-    let data = {};
-    ageBins.forEach(bin => {
-        data[bin] = { 'Male': { 'Diabetic': 0, 'Non-Diabetic': 0 }, 'Female': { 'Diabetic': 0, 'Non-Diabetic': 0 } };
-    });
-
-    // Populate the data structure
-    filteredData.forEach(d => {
-        const age = +d.Age;
-        let bin = null;
-
-        if (age <= 20) bin = "0-20";
-        else if (age <= 40) bin = "20-40";
-        else if (age <= 60) bin = "40-60";
-        else if (age <= 80) bin = "60-80";
-        else bin = "80-100";
-
-        if (+d.Diabetes_012 === 1) {
-            data[bin][d.Sex]['Diabetic']++;
-        } else {
-            data[bin][d.Sex]['Non-Diabetic']++;
+    // Filter out invalid entries
+    const validData = rawData.filter(d => !isNaN(d.Diabetes_012) && d.Sex);
+    
+    // Group by sex and diabetes status
+    const grouped = d3.group(validData, d => d.Sex, d => d.Diabetes_012);
+    
+    // Transform data for chart
+    const data = [];
+    for (let [sex, diabetesMap] of grouped.entries()) {
+        for (let [status, individuals] of diabetesMap.entries()) {
+            const type = (status === 1 || status === 2) ? 'Diabetic' : 'Non-Diabetic';
+            data.push({sex, type, count: individuals.length});
         }
-    });
-
+    }
     return data;
 }
 
 
 function renderScene1(raw_data) {
+    const data = preprocessData(raw_data);
     const svg = d3.select("#slide-container svg");
-
     const width = 800;
     const height = 400;
     const margin = {top: 20, right: 20, bottom: 40, left: 50};
 
-    // Scales
     const xScale = d3.scaleBand()
-    .domain(ageBins)
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+        .domain(data.map(d => d.sex))
+        .range([margin.left, width - margin.right])
+        .padding(0.5);
 
     const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d[1].reduce((acc, curr) => acc + curr[1], 0))])
+        .domain([0, d3.max(data, d => d.count)])
         .range([height - margin.bottom, margin.top]);
 
     const colorScale = d3.scaleOrdinal()
-        .domain(['Male', 'Female'])
-        .range(['#1f77b4', '#ff7f0e']); // blue and orange
+        .domain(['Diabetic', 'Non-Diabetic'])
+        .range(['#d62728', '#2ca02c']);  // Red for Diabetic, Green for Non-Diabetic
 
+    // Create bars
+    svg.selectAll(".bar")
+        .data(data)
+        .join("rect")
+        .attr("class", "bar")
+        .attr("x", d => xScale(d.sex))
+        .attr("y", d => yScale(d.count))
+        .attr("width", xScale.bandwidth() / 2)
+        .attr("height", d => height - margin.bottom - yScale(d.count))
+        .attr("fill", d => colorScale(d.type))
+        .attr("transform", d => `translate(${d.type === 'Diabetic' ? 0 : xScale.bandwidth() / 2},0)`);
+
+    // Axes
     svg.append("g")
         .attr("transform", `translate(0,${height - margin.bottom})`)
         .call(d3.axisBottom(xScale).tickSizeOuter(0));
@@ -113,58 +110,6 @@ function renderScene1(raw_data) {
     svg.append("g")
         .attr("transform", `translate(${margin.left},0)`)
         .call(d3.axisLeft(yScale));
-
-    const processedData = preprocessData(raw_data);
-    const data = [];
-
-    for (const [age, sexes] of Object.entries(processedData)) {
-        for (const [sex, diabeticStatus] of Object.entries(sexes)) {
-            data.push({
-                age: age,
-                sex: sex,
-                count: diabeticStatus['Diabetic'],
-                type: 'Diabetic'
-            });
-            data.push({
-                age: age,
-                sex: sex,
-                count: diabeticStatus['Non-Diabetic'],
-                type: 'Non-Diabetic'
-            });
-        }
-    }
-
-    svg.selectAll(".bar")
-    .data(data)
-    .join("rect")
-    .attr("class", "bar")
-    .attr("x", d => xScale(d.age))
-    .attr("y", d => yScale(d.count))
-    .attr("width", xScale.bandwidth())
-    .attr("height", d => height - margin.bottom - yScale(d.count))
-    .attr("fill", d => colorScale(d.sex));
-
-// Tooltip behavior
-const tooltip = d3.select("#tooltip");
-svg.selectAll(".bar")
-    .on("mouseover", function(event, d) {
-        d3.select(this).attr("opacity", 0.7);
-
-        const totalCount = rawData.filter(row => row.Age === d.age && row.Sex === d.sex).length;
-        const percentage = (d.count / totalCount) * 100;
-
-        tooltip.html(`Age: ${d.age}<br>Sex: ${d.sex}<br>Percentage: ${percentage.toFixed(2)}%`)
-            .style("left", (event.pageX + 10) + "px")
-            .style("top", (event.pageY - 28) + "px")
-            .style("visibility", "visible")
-            .style("background-color", "white")
-            .style("padding", "5px")
-            .style("border", "1px solid black");
-    })
-    .on("mouseout", function(event, d) {
-        d3.select(this).attr("opacity", 1);
-        tooltip.style("visibility", "hidden");
-    });
 }
 
 function renderScene2(data) {
